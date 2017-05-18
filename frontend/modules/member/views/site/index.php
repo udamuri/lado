816e51d063820e2cd76da9860e598991841d9ef8
muri.budiman <muri.budiman@redawning.com>
2017-05-18 12:59:12 +0700

    s

--- frontend/modules/member/views/site/index.php
+++ frontend/modules/member/views/site/index.php
@@ -10,10 +10,32 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Setting Profil';
 $this->params['breadcrumbs'][] = $this->title;
 $act_p = '';
+$act_pa = '';
+$act_po = '';
+$act_me = '';
 if($type == 'profil')
 {
 	$act_p = 'class="active"';
 }
+else if($type == 'password')
+{
+	$act_pa = 'class="active"';
+}
+else if($type == 'photo')
+{
+	$act_po = 'class="active"';
+}
+else
+{
+	$act_p = 'class="active"';
+}
+
+
+if($type == 'message')
+{
+	$act_me = 'class="active"';
+}
+
 ?>
 <div class="container">
     <div class="panel panel-default">
@@ -21,10 +43,10 @@ if($type == 'profil')
             <div class="row">
                 <div class="col-lg-12">
                     <ul class="nav nav-pills" role="tablist">
-					    <li role="presentation" <?=$act_p;?> ><a href="#">Profil</a></li>
-					    <li role="presentation"><a href="#">Ganti Password</a></li>
-					    <li role="presentation"><a href="#">Foto</a></li>
-					    <li role="presentation"><a href="#">Pesan <span class="badge">3</span></a></li>
+					    <li role="presentation" <?=$act_p;?> ><a href="<?=Yii::$app->homeUrl?>profile">Profil</a></li>
+					    <li role="presentation" <?=$act_pa;?> ><a href="<?=Yii::$app->homeUrl?>profile/password">Ganti Password</a></li>
+					    <li role="presentation" <?=$act_po;?>><a href="<?=Yii::$app->homeUrl?>profile/photo">Foto</a></li>
+					    <li role="presentation" <?=$act_me;?> ><a href="<?=Yii::$app->homeUrl?>profile/message">Pesan <span class="badge">3</span></a></li>
 					</ul>
 					<hr>
 					<?php if($type == 'profil') { ?>
