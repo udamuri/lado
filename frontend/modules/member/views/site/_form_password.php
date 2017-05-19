8a77c21b7646553d2c66919c9fd690f33c4cd29f
muri.budiman <muri.budiman@redawning.com>
2017-05-19 13:01:54 +0700

    form password

---
+++ frontend/modules/member/views/site/_form_password.php
@@ -0,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+?>
+
+<?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+    <?= $form->field($model, 'password')->textInput(['autofocus' => true, 'class'=>'form-control input-lg']) ?>
+
+    <?= $form->field($model, 'new_password')->passwordInput(['class'=>'form-control input-lg']) ?>
+    
+    <?= $form->field($model, 'password_repeat')->passwordInput(['class'=>'form-control input-lg']) ?>
+
+    <div class="clearfix"></div>
+
+    <div class="form-group">
+        <?= Html::submitButton('Simpan Perubahan', ['class' => 'btn btn-primary btn-lg', 'name' => 'update-profile']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
+
