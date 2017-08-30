d677cda668b9ff0193c37da696c0c4ecbd59e0ef
muri.budiman <muri.budiman@redawning.com>
2017-08-30 12:51:29 +0700

    lado api

--- frontend/modules/api/controllers/SiteController.php
+++ frontend/modules/api/controllers/SiteController.php
@@ -3,18 +3,43 @@
 namespace app\modules\api\controllers;
 
 use yii\web\Controller;
+use yii\web\Request;
+use yii\web\Response;
+use yii\helpers\Json;
+use yii\filters\auth\CompositeAuth;
+use frontend\modules\member\models\Member;
 
 /**
- * Default controller for the `api` module
+ * Site controller for the `api` module
  */
-class DefaultController extends Controller
+class SiteController extends Controller
 {
+
+    // public function behaviors(){
+    //     $behaviors = parent::behaviors();
+    //     $behaviors['authenticator'] = [
+    //         'class' => CompositeAuth::className(),
+    //     ];
+    //     return $behaviors;
+    // }
+
+	protected function verbs()
+    {
+       return [
+           'index' => ['GET'],
+       ];
+    }
+
     /**
      * Renders the index view for the module
      * @return string
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $examples = Member::find()->all();
+        return [
+            'data'=>$examples,
+        ];
     }
 }
